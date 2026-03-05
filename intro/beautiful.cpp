ef06ae57e6ad4ecf248507fae34aa8ecf8c4f15a
GreenMarioh <mohnishkumar360@gmail.com>
2026-03-05 22:45:31 +0530

    permutations

---
+++ intro/beautiful.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define co cout <<
+#define ci cin >>
+
+#define ded << "\n";
+
+#define vi vector<int>
+#define vll vector<long long>
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+#define meow                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define per(i, a, b) for (int i = b - 1; i >= a; --i)
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    int n;
+    ci n;
+    if (n < 4 && n != 1) {
+        co "NO SOLUTION"; return;
+    }
+    int i = 2;
+    while( i<= n){
+        co i << " ";
+        i+=2;
+    }
+    i = 1;
+    while(i <= n){
+        co i << " ";
+        i+=2;
+    }
+    
+
+}
+
+int main()
+{
+    meow;
+    solve();
+    return 0;
+}
---
+++ intro/increasing.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define co cout <<
+#define ci cin >>
+
+#define ded << "\n";
+
+#define vi vector<int>
+#define vll vector<long long>
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+#define meow                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define per(i, a, b) for (int i = b - 1; i >= a; --i)
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    int n;
+    ci n;
+    vector<int> arr(n);
+    rep(i, 0, n) ci arr[i];
+    int last = arr[0];
+    ll res = 0;
+    rep(i, 1, n){
+        int tmp = -1;
+        if (arr[i] < last){
+            tmp = last;
+            res += last - arr[i];
+        }   
+        last = max(tmp, arr[i]);
+    }
+    co res ded
+}
+
+int main()
+{
+    meow;
+        solve();
+  
+    return 0;
+}
